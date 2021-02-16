e89214559b2095e5d59171cbfdef2940c9853db2
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-02-16 09:13:07 -0500

    Update Volume.java
    Organized structure to be more readable

--- lab2/Volume.java
+++ lab2/Volume.java
@@ -7,53 +7,49 @@
 import java.util.Scanner; // To be able to read from keyboard
 public class Volume {
     public static void main (String [] args) {
-//     Create  a Scanner object to read from the keyboard
-    Scanner keyboard = new Scanner(System.in);
-//    Identifier declarations
-    final double PI = 3.1415;
-    String shapeInput;
-    double radius;
-    double height;
-    double sphereVolume;
-    double cylinderVolume;
-    double coneVolume;
-
-//    Ask user which shape they want to calculate and store input
-    System.out.print("Do you want to calculate the volume of a 1)sphere, 2) cylinder, or 3) cone?: ");
-    shapeInput = keyboard.nextLine();
-//    Run a if statement that calculates chosen shape and outputs volume
-//        Find volume of sphere
-    if (shapeInput.equals("sphere") || shapeInput.equals("1")) {
-        System.out.print("What's the radius?: ");
-        radius = keyboard.nextDouble();
-//      Ask prof why (4/3) is ignored
-        sphereVolume = (4.0/3.0) * PI * (Math.pow(radius, 3));
-        System.out.print("The volume of this sphere is "+ sphereVolume);
-    }
-//    Find volume of cylinder
-    else if (shapeInput.equals("cylinder") || shapeInput.equals("2")) {
-        System.out.print("What's the radius?: ");
-        radius = keyboard.nextDouble();
-        System.out.print("What's the height?: ");
-        height = keyboard.nextDouble();
-        cylinderVolume = PI * (Math.pow(radius, 2) * height);
-        System.out.print("The volume of this sphere is "+ cylinderVolume);
-    }
-//  Find volume cone
-    else if (shapeInput.equals("cone") || shapeInput.equals("3")) {
-        System.out.print("What's the radius?: ");
-        radius = keyboard.nextDouble();
-        System.out.print("What's the height?: ");
-        height = keyboard.nextDouble();
-        coneVolume = PI * Math.pow(radius, 2) * (height/3);
-        System.out.print("The volume of this sphere is "+ coneVolume);
-    }
-//    Inform and end program if user makes incorrect choice
-    else {
-        System.out.print("That's not an option, ending program.");
-    }
-
-//    System.out.print(shapeInput);
-// || shapeInput.startsWith("sphere")
+    //  Create  a Scanner object to read from the keyboard
+        Scanner keyboard = new Scanner(System.in);
+    //  Identifier declarations
+        final double PI = 3.1415;
+        String shapeInput;
+        double radius;
+        double height;
+        double sphereVolume;
+        double cylinderVolume;
+        double coneVolume;
+    //  Ask user which shape they want to calculate and store input
+        System.out.print("Do you want to calculate the volume of a 1)sphere, 2) cylinder, or 3) cone?: ");
+        shapeInput = keyboard.nextLine();
+    //  Run a if statement that calculates chosen shape and outputs volume
+    //  Find volume of sphere
+        if (shapeInput.equals("sphere") || shapeInput.equals("1")) {
+            System.out.print("What's the radius?: ");
+            radius = keyboard.nextDouble();
+    //  Ask prof why (4/3) is ignored
+            sphereVolume = (4.0/3.0) * PI * (Math.pow(radius, 3));
+            System.out.print("The volume of this sphere is "+ sphereVolume);
+        }
+    //  Find volume of cylinder
+        else if (shapeInput.equals("cylinder") || shapeInput.equals("2")) {
+            System.out.print("What's the radius?: ");
+            radius = keyboard.nextDouble();
+            System.out.print("What's the height?: ");
+            height = keyboard.nextDouble();
+            cylinderVolume = PI * (Math.pow(radius, 2) * height);
+            System.out.print("The volume of this sphere is "+ cylinderVolume);
+        }
+    //  Find volume of cone
+        else if (shapeInput.equals("cone") || shapeInput.equals("3")) {
+            System.out.print("What's the radius?: ");
+            radius = keyboard.nextDouble();
+            System.out.print("What's the height?: ");
+            height = keyboard.nextDouble();
+            coneVolume = PI * Math.pow(radius, 2) * (height/3);
+            System.out.print("The volume of this sphere is "+ coneVolume);
+        }
+    //  Inform and end program if user makes incorrect choice
+        else {
+            System.out.print("That's not an option, ending program.");
+        }
     }
 }
