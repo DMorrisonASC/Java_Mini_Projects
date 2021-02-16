a0b51011f54d28e7f0093b4e09d0dd96d99e1448
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-02-16 13:23:47 -0500

    Add files via upload

---
+++ lab2/Errors.java
@@ -0,0 +1,25 @@
+/* Author: Daeshaun Morrison, Muhlenberg College class of 2024(daeshaunkmorrison@gmail.com)
+ Date: 2/15/2020
+ Instructor: Professor Hellsing
+ Description: This program is a for user to debug the following code. Here's the original, undebugged code:
+
+import Math;
+public Errors
+{
+    public void main(String[])
+    {
+    System.out.println("This code contains multiple errors.")
+    System.out.print("These need to be fixed before you submit it.');
+    System.out.println(pow(2,4));
+    System.our.println("Fixed by [ENTER YOUR NAME HERE].");
+    }
+*/
+import java.lang.Math;
+public class Errors {
+    public static void main(String[] args) {
+        System.out.println("This code contains multiple errors.");
+        System.out.println("These need to be fixed before you submit it.");
+        System.out.println(Math.pow(2, 4));
+        System.out.println("Fixed by Daeshaun Morrison.");
+    }
+}
