58b5741e89f27345fd352c4968cc337e2ffe2b2e
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-02-23 11:26:41 -0500

    Added comments to Grades.java

--- lab3/Grades.java
+++ lab3/Grades.java
@@ -3,7 +3,6 @@
  Instructor: Professor Hellsing
  Description: This program is calculating averages.
 */
-import java.util.Locale;
 import java.util.Scanner; // To be able to read from keyboard
 public class Grades {
     public static void main(String[] args) {
@@ -16,6 +15,7 @@ public class Grades {
         double finalGrade = 0;
         boolean continueLoop = true;
         String keepGoing;
+//        Greeting message!
         System.out.println("This is a grade average calculator. Only use numbers from 0-100!");
 //        Ask user to input grades(0-100).
         do {
@@ -25,27 +25,30 @@ public class Grades {
                     System.out.println("That's not a valid input! Pick another number: ");
                     gradeInput = scan.nextDouble();
             }
+//            Add input to the total grades and count the number of grades
             totalGrades = totalGrades + gradeInput;
             numOfGrades++;
+//            Ask if they want to add another grade
             System.out.println("Add another grade? \"Yes\" or \"No\"?");
             keepGoing = scan.next().toLowerCase();
-
+//          Check if answer is starts with "y" or "n". So that answers like "Yes", "Yup", "No" and "Nope" are accepted.
             while (!(keepGoing.startsWith("y") || keepGoing.startsWith("n"))) {
                 System.out.println("That's not \"Yes\" or \"No\". ");
                 keepGoing = scan.next().toLowerCase();
             }
-
+//            If "yes", re-loop
             if (keepGoing.startsWith("y")) {
                 System.out.println("Put in another grade: ");
                 continueLoop = true;
             }
+//            If "no", print out the average and stop loop.
             else {
                 finalGrade = (totalGrades / numOfGrades);
                 System.out.println("Your grade average is " + finalGrade);
                 continueLoop = false;
             }
         }
-//      Keep asking until keepGoing = false
+//      Keep asking until continueLoop = false
         while (continueLoop);
     }
 }
