a822b98fc3a1e5278b872585b0494d7bebc545b1
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-03-03 20:55:04 -0500

    Update DNA2RNA.java

--- lab4/DNA2RNA.java
+++ lab4/DNA2RNA.java
@@ -8,29 +8,49 @@
 import java.util.Scanner;
 
 public class DNA2RNA {
-    public static void main(String[] args) {
-        CheckDNASeq(GetDNASeq());
 
+    public static void main(String[] args) {
+        String getDNA = getDNASeq();
+        boolean checkDNA = checkDNASeq(getDNA);
+        String convertToRNA = convertDNA(getDNA);
+
+        if (checkDNA == true) {
+            System.out.println(convertToRNA);
+        }
+        else {
+            System.out.println("Your DNA sequences contains 1 or more letters that are not A,C,G or T. " +
+            "Ending program.");
+        }
     }
 
-    public static String GetDNASeq(){
+    public static String getDNASeq(){
         Scanner scan = new Scanner(System.in); // Scanner for user input
         String DNAInput = new String();
         System.out.println("What's the DNA sequence?: ");
-        DNAInput = scan.next();
+        DNAInput = scan.next().toUpperCase();
 
         return DNAInput;
     }
 
-    public static String CheckDNASeq(String DNA){
+    public static boolean checkDNASeq(String DNA){
         String DNASeq = DNA;
+        boolean validDNASeq = true;
 
         for (int i = 0; i < DNASeq.length(); i++) {
             char letter = DNASeq.charAt(i);
 
+            if (letter == 'A' || letter == 'C' || letter == 'G' || letter == 'T') {
+                ;
+            }
+            else {
+                validDNASeq = false;
+            }
         }
-        System.out.println(DNASeq);
-        return DNA;
+        return validDNASeq;
+    }
+    public static String convertDNA(String DNA) {
+        String RNASeq = DNA.replace('T', 'U');
+        return RNASeq;
     }
 
 }
