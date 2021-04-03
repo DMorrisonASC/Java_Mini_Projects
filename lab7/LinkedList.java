ae87630ab7ae063630d5989cf12b180098eec672
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-04-02 21:43:53 -0400

    Update LinkedList.java

--- lab7/LinkedList.java
+++ lab7/LinkedList.java
@@ -7,94 +7,39 @@ class to store the first and last node in the chain and functions associated wit
 `Library` class that will be using a `LinkedList` to keep track of the titles of books in the library. The user
 should be able to insert new titles, search for titles, remove titles, and output all of the titles in the library.
 */
-// A class named LinkedList to store multiple book titles. This
-//class should contain
-public class LinkedList {
-    private Node head;
-    private Node tail;
+public class Node {
+private String titleOfBook;
+private Node nextNode;
+private Node previousNode;
 
-    public LinkedList() {
-        head = null;
+public Node(String title, Node nextNode, Node prevNode) {
+ titleOfBook = title;
+ this.nextNode = nextNode;
+ previousNode = prevNode;
+
+}
+
+    public String getTitleOfBook() {
+        return titleOfBook;
     }
-//A method that takes in a String representing a book title as a parameter and inserts the title
-//into the Linked List at the end
-    public void insert(String title) {
-        Node current;
 
-        if(head == null)
-        {
-            Node newNode = new Node(title, head, tail);
-            head = newNode;
-        }
-        else
-        {
-            current = head;
-
-            while(current.getNextNode() != null)
-            {
-                current = current.getNextNode();
-            }
-
-            Node newNode = new Node(title, head, tail);
-            newNode.setTitleOfBook(title);
-            newNode.setNextNode(null);
-
-            current.setNextNode(newNode);
-        }
-
+    public void setTitleOfBook(String titleOfBook) {
+        this.titleOfBook = titleOfBook;
     }
-//    A method that takes in a String representing a book title as a parameter and searches for the
-//    title is in the LinkedList. The method should return true if the title was in the Linked List
-//    and false if not
-    public boolean search(String title) {
-        Node current = head;
 
-        while (current != null) {
-            if (current.getTitleOfBook().equals(title)) {
-                return true;
-            }
-            current = current.getNextNode();
-        }
-        return false;
+    public Node getNextNode() {
+        return nextNode;
     }
-//    A method that takes in a String representing a book title as a parameter and deletes the title
-//in the LinkedList. The method should return true if the title was successfully deleted from
-//the LinkedList and false if not
-    public boolean remove(String title)
-    {
-        if(head == null)
-        {
-            return false;
-        }
-        else if(head.getTitleOfBook() == title)
-        {
-            head = head.getNextNode();
-        }
-        else
-        {
-            Node current = head.getNextNode();
-            Node previous = head;
 
-            while(current != null)
-            {
-                if(current.getTitleOfBook().equals(title))
-                {
-                    previous.setNextNode(current.getNextNode());
-                    return true;
-                }
-                current = current.getNextNode();
-                previous = previous.getNextNode();
-            }
-        }
-        return false;
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
     }
-    public void outputTitles(){
-        Node current = head;
 
-        while (current != null) {
-            System.out.println("\"" + current.getTitleOfBook() + "\"");
-            current = current.getNextNode();
-        }
+    public Node getPreviousNode() {
+        return previousNode;
+    }
 
+    public void setPreviousNode(Node previousNode) {
+        this.previousNode = previousNode;
     }
 }
