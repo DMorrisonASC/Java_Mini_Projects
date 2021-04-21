fb65df6a5ab003aa915dced9d3dc0bcbb29c30e8
DMorrisonASC <52674435+DMorrisonASC@users.noreply.github.com>
2021-04-20 20:18:25 -0400

    Add files via upload

---
+++ Project_2/Field.java
@@ -0,0 +1,89 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+/*
+    Author: Daeshaun Morrison, Muhlenberg College class of 2024(daeshaunkmorrison@gmail.com)
+    Date: 4/18/2021
+    Instructor: For a bit of fungi biology, many common
+mushrooms grows from spores, which are released from adult mushrooms. Most mushrooms take a few days
+to a little over a week to fully mature into adults. Your simulation will need to take this into account.
+Your simulation should read in a file, specified by the user, which determines how long a mushroom lives,
+where the initial spore is placed in the field, and what are the nutrient levels within the field. Then your
+program should simulate one day at a time, as the mushrooms grow, mature, spread their spores to make
+new mushrooms, and die off. Once there are no more mushrooms in the field, the program should specify
+what day had the greatest number of mushrooms at once, how many mushrooms were there on that day, and
+what the nutrient levels in the field are now.
+*/
+public class Field {
+
+    public Field() {
+    }
+
+    public static void createField() throws IOException {
+        System.out.println("What's the name of the file?: ");
+        Scanner scan = new Scanner(System.in);
+        File fileName = new File(scan.nextLine());
+
+        while (fileName.exists() == false) {
+            System.out.println("File does not exist. What's the name of the file to load?: ");
+            fileName = new File(scan.nextLine());
+        }
+        Scanner myFile = new Scanner(fileName);
+
+        int numOfRows = 0;
+        int numOfColumns = 0;
+
+        int lifeSpanNum = 0;
+        int x_cord = 0;
+        int y_cord = 0;
+
+//
+        String line = myFile.nextLine();
+        Scanner stringReader = new Scanner(line);
+        stringReader.useDelimiter("=");
+
+        stringReader.next();
+        lifeSpanNum = stringReader.nextInt();
+
+        System.out.println(lifeSpanNum);
+
+        line = myFile.nextLine();
+        stringReader = new Scanner(line);
+        stringReader.useDelimiter("=|,");
+
+        stringReader.next();
+        x_cord = stringReader.nextInt();
+        y_cord = stringReader.nextInt();
+
+        System.out.println(x_cord);
+        System.out.println(y_cord);
+
+        while (myFile.hasNextLine()) {
+            line = myFile.nextLine();
+            stringReader = new Scanner(line);
+            stringReader.useDelimiter(",");
+
+//            Count amount of columns in mound
+            numOfColumns++;
+
+            while (stringReader.hasNext()) {
+//                stringReader.next();
+                System.out.println(stringReader.next());
+                numOfRows++;
+            }
+        }
+        myFile.close();
+
+        //        Inits vars for field
+        Mound[][] fieldLayout = new Mound[numOfRows/numOfColumns][numOfColumns];
+
+        for (int i = 0; i < fieldLayout.length; i++) {
+            for (int j = 0; j < fieldLayout[i].length; j++) {
+                System.out.print("M0");
+            }
+            System.out.println();
+        }
+        System.out.println(11);
+    }
+}
---
+++ Project_2/Mound.java
@@ -0,0 +1,37 @@
+/*
+    Author: Daeshaun Morrison, Muhlenberg College class of 2024(daeshaunkmorrison@gmail.com)
+    Date: 4/18/2021
+    Instructor: For a bit of fungi biology, many common
+mushrooms grows from spores, which are released from adult mushrooms. Most mushrooms take a few days
+to a little over a week to fully mature into adults. Your simulation will need to take this into account.
+Your simulation should read in a file, specified by the user, which determines how long a mushroom lives,
+where the initial spore is placed in the field, and what are the nutrient levels within the field. Then your
+program should simulate one day at a time, as the mushrooms grow, mature, spread their spores to make
+new mushrooms, and die off. Once there are no more mushrooms in the field, the program should specify
+what day had the greatest number of mushrooms at once, how many mushrooms were there on that day, and
+what the nutrient levels in the field are now.
+*/
+
+import java.util.LinkedList;
+
+public class Mound {
+    private int numOfNutrient;
+    private int numOfSpores;
+    LinkedList<Mushroom> mushroomsInMound = new LinkedList<Mushroom>();
+
+    public Mound(int nutrients) {
+        this.numOfNutrient = nutrients;
+    }
+
+    public Mound(Mushroom mushroom){
+        LinkedList<Mushroom> mushroomsInMound = new LinkedList<Mushroom>();
+        mushroomsInMound.add(mushroom);
+    }
+
+    public static void addSpores(int amountOfNutrients) {
+        if (amountOfNutrients > 0) {
+
+        }
+    }
+
+}
